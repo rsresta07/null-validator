70a4920060d36bb29023122a6b9995b5b2dea559
Rameshwor Shrestha <111293816+rsresta07@users.noreply.github.com>
2025-12-31 15:27:19 +0545

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function isNull(value: unknown): string | null;
-export declare function isUndefined(value: unknown): string | null;
-export declare function isNullish(value: unknown): string | null;
-export declare function isNotNullish<T>(value: T): string | null;
-export declare function isEmptyString(value: unknown): string | null;
